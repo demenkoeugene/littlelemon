54cdeb959a900045d3938a4a48ed2cdad9e983f6
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-06-08 23:24:17 +0300

    initial commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Eugene Demenko on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
