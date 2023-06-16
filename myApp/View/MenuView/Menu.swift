d5db9c04b1bddcabc003029ada1e60844b2c5cb1
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-06-16 14:35:25 +0300

    Organisation code(st2)

--- myApp/View/MenuView/Menu.swift
+++ myApp/View/MenuView/Menu.swift
@@ -46,9 +46,13 @@ struct Menu: View {
             Divider()
            
             FetchedObjects(
-                predicate: buildPredicate(),
-                sortDescriptors: buildSortDescriptors()
-            ) { (dishes: [DishEntity]) in
+                predicate: MenuHelpers.buildPredicate(searchText: searchText,
+                                                      startersIsEnabled: startersIsEnabled,
+                                                      mainsIsEnabled: mainsIsEnabled,
+                                                      dessertsIsEnabled: dessertsIsEnabled,
+                                                      drinksIsEnabled: drinksIsEnabled),
+                sortDescriptors: MenuHelpers.buildSortDescriptors()
+            )  { (dishes: [DishEntity]) in
                 ScrollView {
                     ForEach(dishes) { dish in
                         NavigationLink(destination: MenuItemDetailsView(menuItem: dish)){
@@ -64,82 +68,11 @@ struct Menu: View {
         }
         .onAppear {
             if !isLoaded {
-                getMenuData()
+                MenuHelpers.getMenuData(viewContext: viewContext)
                 isLoaded = true
             }
         }
         
-    }
-    
-    
-    
-    func buildSortDescriptors() -> [NSSortDescriptor] {
-            return [NSSortDescriptor(key: "title", ascending: true, selector: #selector(NSString.localizedStandardCompare))]
-    }
-    
-
-    func buildPredicate() -> NSPredicate {
-        var subpredicates: [NSPredicate] = []
-        
-        // Search text predicate
-        if !searchText.isEmpty {
-            let searchPredicate = NSPredicate(format: "title CONTAINS[cd] %@", searchText)
-            subpredicates.append(searchPredicate)
-        }
-        
-        // Category filters
-        if !startersIsEnabled {
-            let startersPredicate = NSPredicate(format: "category != %@", "starters")
-            subpredicates.append(startersPredicate)
-        }
-        if !mainsIsEnabled {
-            let mainsPredicate = NSPredicate(format: "category != %@", "mains")
-            subpredicates.append(mainsPredicate)
-        }
-        if !dessertsIsEnabled {
-            let dessertsPredicate = NSPredicate(format: "category != %@", "desserts")
-            subpredicates.append(dessertsPredicate)
-        }
-        if !drinksIsEnabled {
-            let drinksPredicate = NSPredicate(format: "category != %@", "drinks")
-            subpredicates.append(drinksPredicate)
-        }
-        
-        // Combine subpredicates using AND operator
-        return NSCompoundPredicate(andPredicateWithSubpredicates: subpredicates)
-    }
-
-    func getMenuData() {
-        PersistenceController.shared.clear()
-        let serverURLString = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
-        let url = URL(string: serverURLString)
-        guard let url = url else {
-            print("Invalid server URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data{
-                do {
-                    let decoder = JSONDecoder()
-                    let menuList = try decoder.decode(MenuList.self, from: data)
-                    
-                    for menuItem in menuList.menu {
-                        let dish = DishEntity(context: viewContext)
-                        dish.title = menuItem.title
-                        dish.price = menuItem.price
-                        dish.image = menuItem.image
-                        dish.descriptionItem = menuItem.descriptionItem
-                        dish.category = menuItem.category
-                    }
-                    try? viewContext.save()
-                } catch {
-                    print("Error decoding menu data: \(error)")
-                }
-            }
-        }
-        task.resume()
     }
 }
 
---
+++ myApp/ViewModel/ViewModel.swift
@@ -0,0 +1,79 @@
+//
+//  ViewModel.swift
+//  myApp
+//
+//  Created by Eugene Demenko on 16.06.2023.
+//
+
+import Foundation
+import CoreData
+
+struct MenuHelpers {
+    static func buildSortDescriptors() -> [NSSortDescriptor] {
+        return [NSSortDescriptor(key: "title", ascending: true, selector: #selector(NSString.localizedStandardCompare))]
+    }
+
+    static func buildPredicate(searchText: String, startersIsEnabled: Bool, mainsIsEnabled: Bool, dessertsIsEnabled: Bool, drinksIsEnabled: Bool) -> NSPredicate {
+        var subpredicates: [NSPredicate] = []
+        
+        // Search text predicate
+        if !searchText.isEmpty {
+            let searchPredicate = NSPredicate(format: "title CONTAINS[cd] %@", searchText)
+            subpredicates.append(searchPredicate)
+        }
+        
+        // Category filters
+        if !startersIsEnabled {
+            let startersPredicate = NSPredicate(format: "category != %@", "starters")
+            subpredicates.append(startersPredicate)
+        }
+        if !mainsIsEnabled {
+            let mainsPredicate = NSPredicate(format: "category != %@", "mains")
+            subpredicates.append(mainsPredicate)
+        }
+        if !dessertsIsEnabled {
+            let dessertsPredicate = NSPredicate(format: "category != %@", "desserts")
+            subpredicates.append(dessertsPredicate)
+        }
+        if !drinksIsEnabled {
+            let drinksPredicate = NSPredicate(format: "category != %@", "drinks")
+            subpredicates.append(drinksPredicate)
+        }
+        
+        // Combine subpredicates using AND operator
+        return NSCompoundPredicate(andPredicateWithSubpredicates: subpredicates)
+    }
+
+    static func getMenuData(viewContext: NSManagedObjectContext) {
+        PersistenceController.shared.clear()
+        let serverURLString = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        let url = URL(string: serverURLString)
+        guard let url = url else {
+            print("Invalid server URL")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    let menuList = try decoder.decode(MenuList.self, from: data)
+                    
+                    for menuItem in menuList.menu {
+                        let dish = DishEntity(context: viewContext)
+                        dish.title = menuItem.title
+                        dish.price = menuItem.price
+                        dish.image = menuItem.image
+                        dish.descriptionItem = menuItem.descriptionItem
+                        dish.category = menuItem.category
+                    }
+                    try? viewContext.save()
+                } catch {
+                    print("Error decoding menu data: \(error)")
+                }
+            }
+        }
+        task.resume()
+    }
+}
