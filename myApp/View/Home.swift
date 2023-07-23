aeec4a583dfc84a1f64ad5cb59daf7dde282c720
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-07-23 22:46:39 +0300

    fix tabview

--- myApp/View/Home.swift
+++ myApp/View/Home.swift
@@ -10,24 +10,50 @@ import SwiftUI
 struct Home: View {
     let persistence = PersistenceController.shared
     @EnvironmentObject var viewModel: AuthViewModel
+    @StateObject var model = Model()
+    
     var body: some View {
-        TabView{
-            Menu()
-                .environment(\.managedObjectContext, persistence.container.viewContext)
-                .tabItem {
-                    Label("Menu", systemImage: "fork.knife.circle")
-                }
-            UserProfile()
-                .navigationTitle("Personal Information")
-                .tabItem {
-                    Label("Profile", systemImage: "person")
-                }
+        NavigationView {
+            TabView(selection: $model.tabViewSelectedIndex) {
+                Menu()
+                    .tag(0)
+                    .environment(\.managedObjectContext, persistence.container.viewContext)
+                    .tabItem {
+                        Label("Menu", systemImage: "fork.knife.circle")
+                    }
+                    .toolbarBackground(.white)
+               
+                LocationView()
+                    .tag(2)
+                    .tabItem {
+                        Label("Locations", systemImage: "fork.knife")
+                    }
+                ReservationView()
+                    .tag(3)
+                    .tabItem {
+                        Label("Reservation", systemImage: "square.and.pencil")
+                    }
+                UserProfile()
+                    .tag(1)
+                    .navigationTitle("Personal Information")
+                    .tabItem {
+                        Label("Profile", systemImage: "person")
+                    }
+                
+            }
+            .onAppear {
+                let tabBarAppearance = UITabBarAppearance()
+                tabBarAppearance.configureWithDefaultBackground()
+                UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+            }
+           
+            .navigationBarBackButtonHidden(true)
         }
         
-        .navigationBarBackButtonHidden(true)
+        .environmentObject(model) // Provide the Model object as an environment object
     }
-    
 }
+
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
